aec0d0c3515ca3fd7a5b5248f8930fc32d3e0490
oPotat <adhamyousry6@gmail.com>
2026-04-21 15:19:10 +0200

    Working on DQN agent itself.

--- DQN_agent.py
+++ DQN_agent.py
@@ -11,8 +11,9 @@ import matplotlib.pyplot as plt
 ENV_NAME = "LunarLander-v3"
 HIDDEN_SIZE = 64
 BUFFER_SIZE = 10000
-
-
+EPSILON_START = 1.0
+LR = 0.001              # Learning Rate
+BATCH_SIZE = 64
 
 class QNetwork(nn.Module):
     def __init__(self, state_dim, action_dim, hidden):
@@ -64,19 +65,43 @@ class ReplayBuffer:
         batch = random.sample(self.buffer, batch_size)
         states, actions, rewards, next_states, dones = zip(*batch)
         return(
-            np.array(states), 
-            np.array(actions), 
-            np.array(rewards), 
-            np.array(next_states), 
-            np.array(dones)
+            torch.FloatTensor(np.array(states)),
+            torch.LongTensor(np.array(actions)),
+            torch.FloatTensor(np.array(rewards)),
+            torch.FloatTensor(np.array(next_states)),
+            torch.FloatTensor(np.array(dones)),
             )
     
     def size (self):
         return len(self.buffer)    
         
 class DQNAgent:
-    def __init__(self):
-        pass
+    def __init__(self, state_dim, action_dim):
+        
+        self.actioin_dim = action_dim
+        self.epsilon = EPSILON_START
+        
+        self.q_net = QNetwork(state_dim, action_dim)
+        self.target = QNetwork(state_dim, action_dim)
+        self.target.load_state_dict(self.q_net.state_dict())
+        self.target.eval()
+        
+        self.optimizer = optim.Adam(self.q_net.parameters(), lr=LR)
+        self.loss = nn.MSELoss()
+     
+     # Epsilon Greedy for actions.   
+    def select_action (self, state):
+        if random.random < self.epsilon: # Exploration
+            return random.randrange(self.actioin_dim)
+        with torch.no_grad(): # Exploration fail so use exploitation
+            q = self.q_net(torch.FloatTensor(state).unsqueeze(0)) 
+        return q.argmax().item()
+    
+    def update(self):
+        if len(self.buffer) < BATCH_SIZE:
+            return
+        states, actions, rewards, next_states, dones = self.buffer.sample(BATCH_SIZE)
+    pass
     
 def train():
     pass
