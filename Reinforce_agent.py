c7b12dae85b2d565b56f7525132e3b2b1083d542
oPotat <adhamyousry6@gmail.com>
2026-04-22 16:03:06 +0200

    Save the reward for the compare and list hyperparamters at the top

--- Reinforce_agent.py
+++ Reinforce_agent.py
@@ -5,8 +5,15 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
+# Hyperparameters
+ENV_NAME = "CartPole-v1"
+EPISODES = 1000
+GAMMA = 0.99
+LR = 0.001
+EPISODES = 500
+
 # Might be subject to change.
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+env = gym.make(ENV_NAME, render_mode="rgb_array")
 n_states = env.observation_space.shape[0]
 n_actions = env.action_space.n
 print(f'Number of states: {n_states}, Number of actions: {n_actions}')
@@ -22,7 +29,7 @@ class PolicyNetwork(nn.Module):
         return torch.softmax(self.fc2(x), dim=-1)
 
 
-def reinforce(env, episodes=1000, gamma=0.99, lr=0.001):
+def reinforce(env, episodes=EPISODES, gamma=GAMMA, lr=LR):
     policy_net = PolicyNetwork(n_states, n_actions)
     optimizer = optim.Adam(policy_net.parameters(), lr=lr)
     
@@ -62,15 +69,17 @@ def reinforce(env, episodes=1000, gamma=0.99, lr=0.001):
         total_reward = sum(rewards)
         reward_history.append(total_reward)
 
-        if episode % 100 == 0:
+        if episode % 100 == 0: # Might tweak it to take average.
             print(f'Episode {episode}: Total Reward: {total_reward}')
 
     
     return policy_net, reward_history
 
 # Run the training and capture the history
-reinforce_agent, reward_history = reinforce(env, episodes=500)
+reinforce_agent, reward_history = reinforce(env, episodes=EPISODES)
 
+np.save("reinforce_rewards.npy", np.array(reward_history)) # I think? Not sure.
+print("Saved reinforce_rewards.npy")
 
 plt.figure(figsize=(10, 6))
 plt.plot(reward_history, color='blue', alpha=0.8)
@@ -78,4 +87,5 @@ plt.title('REINFORCE Learning Curve on CartPole-v1')
 plt.xlabel('Episode')
 plt.ylabel('Total Reward')
 plt.grid(True)
+plt.savefig("reinforce_rewards.png")
 plt.show()
